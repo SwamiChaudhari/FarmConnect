e8f154b0c6e6f7707eb7d3717e67052f8504ea38
swami chaudhari <swami1642004@gmail.com>
2025-01-31 19:35:47 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-dotenv.config({ path: "./env" });
-
-connectDB();
+dotenv.config({ path: "./.env" });
 
 
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    })
+  })
+  .catch((err) => {
+    console.log("mondo db connection failed" , err);
+  });
 
 
 
---
+++ src/utils/ApiError.js
@@ -0,0 +1,30 @@
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "something went wrong", 
+    errors = [],
+    statck = ""
+  )
+    {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.errors = errors;
+    this.success = false;
+
+    if (statck) {
+        this.stack = statck;
+    } else {
+        Error.captureStackTrace(this, this.constructor);
+    }
+
+    if (errors) {
+        this.errors = errors;
+    }
+
+
+  }
+}
+
+module.exports = ApiError;
